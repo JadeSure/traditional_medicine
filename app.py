dff42719db4c92c3e1fb329aae0478d7ad9ae06e
cloudops-shawn <shawn.wang@woodside.com>
2024-07-07 00:19:19 +0800

    saved the preparing email sending

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ import shutil
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
 from email.mime.text import MIMEText
 
 # Load environment variables from .env file
@@ -38,69 +36,103 @@ def read_names(file_path):
     return output
 
 
-def send_email(mail_user, mail_pass, mail_receiver):
-    # QQ Mail SMTP server address and port
-    smtp_server = "smtp.qq.com"
-    smtp_port = 465  # SSL
+# read word docs
+def read_docx(file_path):
+    doc = Document(file_path)
+    for p in doc.paragraphs:
+        print(p.text)
 
-    # Your QQ Mail account and password
-    mail_user = "your-account@qq.com"
-    # If you have enabled SMTP service on QQ Mail, use the authorization code.
-    mail_pass = "your-password"
 
-    # Email content
-    subject = "Hello from Python"
-    content = "This is a test email sent from Python."
-
-    # Create a multipart message
+def send_email(mail_user, app_password, mail_receiver):
+    # setup the parameters of the message
+    password = "yourapppassword"  # replace with your app password
     msg = MIMEMultipart()
-    msg["From"] = mail_user
-    msg["To"] = "receiver@example.com"
-    msg["Subject"] = subject
+    msg['From'] = "youremail@outlook.com"  # replace with your email
+    msg['To'] = "recipientemail@gmail.com"  # replace with recipient's email
+    msg['Subject'] = "Subject of the Email"
 
-    # Attach the email content
-    msg.attach(MIMEText(content, "plain"))
+    # add in the message body
+    message = "body of the email"
+    msg.attach(MIMEText(message, 'plain'))
 
-    # Specify the file path of your attachment
-    file_path = "/path/to/your/file"
+    #create server
+    server = smtplib.SMTP('smtp.office365.com: 587')
 
-    # Create a MIMEBase object for the attachment
-    part = MIMEBase("application", "octet-stream")
-    part.set_payload(open(file_path, "rb").read())
-    encoders.encode_base64(part)
-    part.add_header(
-        "Content-Disposition", 'attachment; filename="%s"' % os.path.basename(file_path)
-    )
+    server.starttls()
 
-    # Attach the file to the email
-    msg.attach(part)
+    # Login Credentials for sending the mail
+    server.login(msg['From'], password)
 
-    try:
-        server = smtplib.SMTP_SSL(smtp_server, smtp_port)
-        server.login(mail_user, mail_pass)
-        server.sendmail(mail_user, ["receiver@example.com"], msg.as_string())
-        server.quit()
-        print("Email sent successfully.")
-    except smtplib.SMTPException as e:
-        print("Error:", e)
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+
+    server.quit()
+
+# import smtplib
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+
+# # setup the parameters of the message
+# password = "yourapppassword"  # replace with your app password
+# msg = MIMEMultipart('alternative')
+# msg['From'] = "youremail@outlook.com"  # replace with your email
+# msg['To'] = "recipient1@gmail.com, recipient2@gmail.com, recipient3@gmail.com"  # replace with recipient's emails
+# msg['Subject'] = "Subject of the Email"
+
+# # create plain text and HTML version of your message
+# text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttps://www.python.org"
+# html = """\
+# <html>
+#   <head></head>
+#   <body>
+#     <p>Hi!<br>
+#        How are you?<br>
+#        Here is the <a href="https://www.python.org">link</a> you wanted.
+#     </p>
+#   </body>
+# </html>
+# """
+
+# # Record the MIME types of both parts - text/plain and text/html.
+# part1 = MIMEText(text, 'plain')
+# part2 = MIMEText(html, 'html')
+
+# # Attach parts into message container.
+# # According to RFC 2046, the last part of a multipart message, in this case
+# # the HTML message, is best and preferred.
+# msg.attach(part1)
+# msg.attach(part2)
+
+# #create server
+# server = smtplib.SMTP('smtp.office365.com: 587')
+
+# server.starttls()
+
+# # Login Credentials for sending the mail
+# server.login(msg['From'], password)
+
+# # send the message via the server.
+# server.sendmail(msg['From'], msg['To'].split(','), msg.as_string())
+
+# server.quit()
 
 
 if __name__ == "__main__":
 
-    time_stamp = get_time_stamp()
+    # time_stamp = get_time_stamp()
 
-    source_res_dir = f"{BASE_PATH}/result"
+    # source_res_dir = f"{BASE_PATH}/result"
     source_origin_dir = f"{BASE_PATH}/source"
-    destination_dir = f"{BASE_PATH}/history/{time_stamp}/result"
+    # destination_dir = f"{BASE_PATH}/history/{time_stamp}/result"
 
-    files = os.listdir(source_res_dir)
-    for file in files:
-        source = os.path.join(source_res_dir, file)
-        destination = os.path.join(destination_dir, file)
+    # files = os.listdir(source_res_dir)
+    # for file in files:
+    #     source = os.path.join(source_res_dir, file)
+    #     destination = os.path.join(destination_dir, file)
 
-        # create the destination directory if it doesn't exist
-        os.makedirs(os.path.dirname(destination), exist_ok=True)
-        shutil.move(source, destination)
+    #     # create the destination directory if it doesn't exist
+    #     os.makedirs(os.path.dirname(destination), exist_ok=True)
+    #     shutil.move(source, destination)
 
     # Get environment variables
     # mail_user = os.getenv('mail_user')
@@ -115,3 +147,8 @@ if __name__ == "__main__":
     # for name in people.keys():
     #     print(type(name))
     #     replace_string_in_docx(source_cert, 'LEI    yao', name, f'{BASE_PATH}/res/{name}')
+
+    
+    presentation_name_docs = f"{source_origin_dir}/2024全年讲座名单.docx"
+    content = read_docx(presentation_name_docs)
+    print(content)
